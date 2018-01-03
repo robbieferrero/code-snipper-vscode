1d510fe8dc126049ca67ff3afad404b86e94b37b
Robbie Ferrero <robbieferrero@gmail.com>
2018-01-02 18:10:59 -0600

    Added file to png support. Needs work

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,31 @@
 'use strict';
 import * as vscode from 'vscode';
+import * as copper from 'code-snipper';
+import * as fs from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
 
     console.log('Code Snipper is now active!');
 
     let disposeCreateImageFromFile: vscode.Disposable = vscode.commands.registerCommand('extension.createImageFromFile', () => {
+        let editor: vscode.TextEditor = vscode.window.activeTextEditor;
+        if (!editor) {
+            vscode.window.showInformationMessage(`There was an issue with the current editor, please try again.`);
+            return;
+        }
+
+        let fileName: string = editor.document.fileName;
+        // TODO: Add output filename support. Add temporary filename support
+        if (!fs.existsSync(fileName)) {
+            vscode.window.showInformationMessage(`Current file path cannot be found. Please save your file if you have not and try again.`);
+            return;
+        }
+        // TODO: Add options
+        let result = copper(fileName);
+        
+        vscode.window.setStatusBarMessage('Image created from file!', 1000);
+    });
+    let disposeCreateImageFromSelection: vscode.Disposable = vscode.commands.registerCommand('extension.createImageFromSelection', () => {
         let editor: vscode.TextEditor = vscode.window.activeTextEditor;
         if (!editor) {
             return;
@@ -15,9 +35,6 @@ export function activate(context: vscode.ExtensionContext) {
         let text: string = editor.document.getText(selection);
 
         vscode.window.showInformationMessage(`Selected characters: ${text}`);
-        vscode.window.setStatusBarMessage('Image created from file!', 1000);
-    });
-    let disposeCreateImageFromSelection: vscode.Disposable = vscode.commands.registerCommand('extension.createImageFromSelection', () => {
         vscode.window.setStatusBarMessage('Image created from selection!', 1000);
     });
 
